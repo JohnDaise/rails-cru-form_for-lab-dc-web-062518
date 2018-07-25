fab3776825660034480898be8d56ea470f8c8808
JohnDaise <jdaise@gmail.com>
2018-07-25 00:40:59 -0400

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,45 @@
 class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(:name)
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(:name)
+    redirect_to genre_path(@genre)
+  end
+
+
+
+
+
+
+  private
+
+
+
+
+
+
+
+
+
 end
